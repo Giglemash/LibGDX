1aeb0dda0f369287ebe2ed49f3169340d076f2ec
Zendux <zendux.bb@gmail.com>
2019-07-15 18:17:52 +0300

    HW2 DOne

--- core/src/ru/geekbrains/stargame/Background.java
+++ core/src/ru/geekbrains/stargame/Background.java
@@ -2,34 +2,25 @@ package ru.geekbrains.stargame;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
+
 
 public class Background {
+
     Texture texture;
-    Vector2 position; // position.x position.y
-    float speed; //
 
-
-    public Background(){
-        texture = new Texture("1162129.jpg");
-
+    public Background() {
+        texture = new Texture("908869.png");
     }
-    public void render (SpriteBatch batch){
-        batch.draw(texture, 0, 0, 1280, 720); //????????
-    }
-    //void draw (Texture texture, float x, float y)????
 
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, 0, 0);
+    }
 
     public void update() {
-        position.x -= speed;
-        if (position.x < -20) {
-            position.x = 1280;
-            position.y = MathUtils.random(0, 720);
-            speed = MathUtils.random(0.5f, 2.5f);
-        }
     }
 }
+
+
 
 
 
--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -1,42 +1,80 @@
 package ru.geekbrains.stargame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Background background;
-	Hero hero;
-	
+
+public class StarGame implements ApplicationListener {
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
+
 	@Override
-	public void create () {
+	public void create() {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
-		background = new Background();
-		hero = new Hero();
+
+		texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+		sprite = new Sprite(texture);
+		sprite.setPosition(w / 2 - sprite.getWidth() / 2, h / 2 - sprite.getHeight() / 2);
 	}
 
 	@Override
-	public void render () {
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+	}
+
+	@Override
+	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10.0f);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10.0f);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateY(-1f);
+			else
+				sprite.translateY(-10.0f);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateY(1f);
+			else
+				sprite.translateY(10.0f);
+		}
 		batch.begin();
-		background.render(batch);
-		hero.render(batch);
+		sprite.draw(batch);
 		batch.end();
 	}
 
-	public void update (){
-		background.update();
-		hero.update();
-	}
-	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
 	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
 }
